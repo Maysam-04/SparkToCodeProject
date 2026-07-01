f350c3979e3ad93546e4ab9dfab899626d89f73d
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-02 01:12:41 +0400

    Added task2 (Rectangle Calculator)

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -5,17 +5,17 @@ class Program
     static void Main(string[] args)
     {
         //Task1:Personal Info Card
-        const String Name = "Adam";
+       /* const String Name = "Adam";
         Console.Write(" Name: " + Name);
         const int Age = 22;
         Console.Write(" Age: " + Age);
         const double Height = 16.9;
         Console.Write(" Height: " + Height);
         const bool Student = true;
-        Console.Write(" Student: " + Student);
+        Console.Write(" Student: " + Student);*/
          
        //Task2:  Rectangle Calculator
-        /*Console.WriteLine("Enter the Lenght of Rectangle:");
+        Console.WriteLine("Enter the Lenght of Rectangle:");
         int length = int.Parse(Console.ReadLine());
         
         Console.WriteLine("Enter the Width of Rectangle:");
@@ -24,7 +24,7 @@ class Program
         int Area = length * Width;
         int Perimeter = 2* (length +Width);
         
-        Console.WriteLine("The area is: " +Area + " The perimeter is: " + Perimeter);*/
+        Console.WriteLine("The area is: " +Area + " The perimeter is: " + Perimeter);
         
        
        
