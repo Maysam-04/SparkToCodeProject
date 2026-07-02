21134caea6eb6ae4c184924c91ad327021e7a91e
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-02 06:42:14 +0400

    Added task 5 (Grade Letter Lookup)

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -43,10 +43,54 @@ class Program
       }*/
        
        //Task 4:Voting Eligibility
+       /*Console.WriteLine("Enter your age:");
+       int age = int.Parse(Console.ReadLine());
+       Console.WriteLine(" Do you have vaild national ID (yes/no)");
+       String nationalID = Console.ReadLine();
+
+       bool validID = nationalID == "yes";
+
+       if (age >= 18 && validID )
+       {
+        Console.WriteLine("eligible to vote");
+       }
+       else 
+       {
+        Console.WriteLine("Not eligible to vote");
+       }*/
        
-       
-       
-       
+       // Task 5:Grade Letter Lookup
+       Console.WriteLine("Enter a grade:");
+       char grade = char.Parse(Console.ReadLine());
+
+       switch (grade)
+       {
+        case 'A':
+         Console.WriteLine("Excellent");
+           break;
+        case 'B':
+         Console.WriteLine("VeryGood");
+           break;
+        case 'C':
+         Console.WriteLine("Good");
+         break;
+        case 'D':
+         Console.WriteLine("pass");
+         break;
+        case 'F':
+         Console.WriteLine("Fail");
+           break;
+        default:
+         Console.WriteLine("invalid grade");
+           break;
+       }
+
+
+
+
+
+
+
 
 
 
