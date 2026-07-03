e49f2ed70fa813adfceb666f0833888bcccc453e
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-04 02:17:11 +0400

    implemented project (Task1 Countdown Timer)

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Task_2;
+﻿using System;
+
+namespace Task_2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 - Countdown Timer
+        Console.Write("Enter a starting number:");
+        int startNumber = int.Parse(Console.ReadLine());
+
+        for (int n = startNumber; startNumber >= 1; startNumber--)
+        {
+            Console.WriteLine( startNumber); 
+        }
+        Console.WriteLine("Liftoff!" );
     }
 }
