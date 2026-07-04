a0dbd662518fd112e9b8e9676aa0a7f36c9cd52e
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-05 03:07:40 +0400

    implemnted project (Task 9 Validated Positive Number Input)

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -84,84 +84,126 @@ class Program
 
         }*/
         ////////////////////////////////////
-        
+
         //Task 6 - Safe Division Calculator
-       /* try
-        {
-            Console.WriteLine("Enter the first number:");
-            int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number:");
-            int secondNumber = int.Parse(Console.ReadLine());
-            
-            int div = firstNumber / secondNumber;
-            Console.WriteLine("Result is :"+div);
-        }
-        catch (DivideByZeroException )
-        {
-            Console.WriteLine( "second number is zero");
-            
-        }
-        catch (FormatException )
-        {
-            Console.WriteLine( "number is invalid");
-        }*/
+        /* try
+         {
+             Console.WriteLine("Enter the first number:");
+             int firstNumber = int.Parse(Console.ReadLine());
+             Console.WriteLine("Enter the second number:");
+             int secondNumber = int.Parse(Console.ReadLine());
+
+             int div = firstNumber / secondNumber;
+             Console.WriteLine("Result is :"+div);
+         }
+         catch (DivideByZeroException )
+         {
+             Console.WriteLine( "second number is zero");
+
+         }
+         catch (FormatException )
+         {
+             Console.WriteLine( "number is invalid");
+         }*/
         //////////////////////////////////////////
-        
+
         //Task 7 - Repeating Menu with Exit Option
-       /* int choice=0;
-        while (choice != 3)
+        /* int choice=0;
+         while (choice != 3)
+         {
+             Console.WriteLine("1.Hello");
+             Console.WriteLine("2.Good Morning");
+             Console.WriteLine("3.exit");
+
+             try
+             {
+                 Console.Write("Enter choice num:");
+             choice = int.Parse(Console.ReadLine());
+
+             switch (choice)
+             {
+                 case 1:
+                     Console.WriteLine("Hello");
+                     break;
+                 case 2:
+                     Console.WriteLine("Good Morning");
+                     break;
+                 case 3:
+                     Console.WriteLine("exit");
+                     break;
+                 default:
+                     Console.WriteLine("Incorrect choice");
+                     break;
+             }
+             }
+             catch (FormatException)
+             {
+                 Console.WriteLine("invalid number");
+             }
+         }*/
+
+        ////////////////////////////////////////////////
+
+        //Task 8 - Sum of Even Numbers Only
+       /* Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int N;
+        for (N = 1; N <= number; N++)
         {
-            Console.WriteLine("1.Hello");
-            Console.WriteLine("2.Good Morning");
-            Console.WriteLine("3.exit");
-            
-            try
-            { 
-                Console.Write("Enter choice num:");
-            choice = int.Parse(Console.ReadLine());
-            
-            switch (choice)
-            {   
-                case 1:
-                    Console.WriteLine("Hello");
-                    break;
-                case 2:
-                    Console.WriteLine("Good Morning");
-                    break;
-                case 3:
-                    Console.WriteLine("exit");
-                    break;
-                default:
-                    Console.WriteLine("Incorrect choice");
-                    break;
+            if (N % 2 == 0)
+            {
+                sum = sum + N;
+
             }
+        }
+        Console.WriteLine("The final sum is : " + sum);*/
+
+      ///////////////////////////////////////////
+      
+      // Task 9 - Validated Positive Number Input
+      int sum = 0;
+      int number=0;
+      int i;
+      bool vaildinput=false;
+        do
+        {
+            try
+            {
+                Console.Write("Enter a positive  number: ");
+                 number = int.Parse(Console.ReadLine());
+                 
+                 if ( number <=0)
+                 {
+                     Console.WriteLine(" you entered a zero or negative number ");
+                    number= int.Parse(Console.ReadLine());
+                 }
+                 else
+                 {
+                     Console.WriteLine(" vaild input true");
+
+                 }
+                
             }
             catch (FormatException)
             {
-                Console.WriteLine("invalid number");
+                Console.WriteLine("Not a valid number");
             }
-        }*/
+            
+        } while ( ! vaildinput);
         
-     ////////////////////////////////////////////////
-     
-     //Task 8 - Sum of Even Numbers Only
-      Console.WriteLine("Enter a number:");
-      int number = int.Parse(Console.ReadLine());
-      int sum = 0;
-      int N;
-      for (N = 1; N <= number; N++)
-      {
-          if (N % 2 == 0)
-          {
-              sum = sum + N;
-          
-          }
-      }
-      Console.WriteLine("The final sum is : " + sum);
+        for ( i = 1; i <=number;i++)
+        {
+            sum = sum + i;
+                
+        }
+        Console.WriteLine(" the final sum is: "+sum);
+    }
+}
 
 
-    }
-    }
+
+
 
 
 
