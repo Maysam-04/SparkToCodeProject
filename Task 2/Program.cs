28628f8825fc859fd73484bade7301be0cbb5c9a
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-04 09:40:01 +0400

    implemented project (Task 2 Sum of Numbers 1 to N)

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -7,17 +7,26 @@ class Program
     static void Main(string[] args)
     {
         // Task 1 - Countdown Timer
-        Console.Write("Enter a starting number:");
+        /*Console.Write("Enter a starting number:");
         int startNumber = int.Parse(Console.ReadLine());
 
         for (int n = startNumber; n >= 1; n--)
         {
             Console.WriteLine(n); 
         }
-        Console.WriteLine("Liftoff!" );
+        Console.WriteLine("Liftoff!" );*/
         ///////////////////////////////////////
         
         // Task 2 - Sum of Numbers 1 to N
-        Console.WriteLine("");
+        Console.Write("Enter a whole number N :");
+        int wholeNumber = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int N; 
+        for ( N = 1; N <= wholeNumber; N++)
+        {
+            sum = sum + N;
+        }
+        Console.WriteLine(sum);
     }
 }
