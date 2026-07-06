6dc1ccdf383bfe0ae7cb4ee024f8a104a78e03d3
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-06 10:14:16 +0400

    Add new project files

---
+++ Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Task 1 - Absolute Difference
+        
+        Console.Write("Enter a  first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter a  second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        
+        int subtraction= num2-num1;
+        
+        int subtractionresult = Math.Abs(subtraction);
+        Console.WriteLine("The final result is :"+subtractionresult);
+        
+    }
+}
+
