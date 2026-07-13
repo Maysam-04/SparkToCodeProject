d0baa2811104d44fecdf08fd36afda10b4fff2ad
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-13 20:24:48 +0400

    created method of deposit

--- OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
+++ OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055a47a552ce3369f8cba24612c539c4b34e4a03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d38c64a60794fe8387a31626ece0008b9e4e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
