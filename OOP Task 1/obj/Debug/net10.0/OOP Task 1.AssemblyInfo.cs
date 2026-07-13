7f031153f6b022b3220580947de26b259c33154a
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-13 20:43:29 +0400

    created method sendemail

--- OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
+++ OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32be8dbc57a02088b2179791744e7eb0f06db1be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+252210d680eb027aaf11a591f451c62811112387")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
