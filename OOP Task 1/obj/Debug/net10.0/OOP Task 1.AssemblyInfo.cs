40632bd4f3c0be0c61a1fb78f7adad4134781a9c
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-14 18:23:49 +0400

    created switch in main

--- OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
+++ OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8168c479a06b02f7edd35c7b83916924fc3b250e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac69fa68c1df34d3cb1f36992756865609716883")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
