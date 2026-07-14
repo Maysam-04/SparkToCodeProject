b2aa753c41da15264ba870329c7067a2f6ace38e
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-14 18:09:47 +0400

    created method Restock(int quantity)

--- OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
+++ OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9a694b2be49cef3f1fc9cabf20dc61c676030f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5db368ce1e698a90acbc6397ce96a3f99e182a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
