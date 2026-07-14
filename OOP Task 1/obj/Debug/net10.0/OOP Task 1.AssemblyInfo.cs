2936029c2d344e18581f165018347630eb8945cb
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-14 18:27:39 +0400

    created Case 7 - Compare Two Account Balances

--- OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
+++ OOP Task 1/obj/Debug/net10.0/OOP Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ba185d0565d54c51030b884147aed5a9f1e785")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5416aa77d5cace236018d9241190aff999811780")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
