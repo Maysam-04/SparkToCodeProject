56f718dc006763d6c92740254b48c2fd2b1b3c41
Maysam alharthi <maysamalharthy5@gmail.com>
2026-07-18 00:45:19 +0400

    Add new project files

---
+++ OOP Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Task_2;
+
+public class Room
+{
+    
+}
+
+public class Guest
+{
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
